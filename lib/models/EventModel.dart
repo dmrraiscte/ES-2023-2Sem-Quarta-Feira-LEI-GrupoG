39f58408f2dfac0d60bf282ac8d7df17cf6ba8e9
Lavian13 <lcava@iscte-iul.pt>
2023-04-07 16:58:28 +0100

    Working only Windows missing unit testing.

--- lib/models/EventModel.dart
+++ lib/models/EventModel.dart
@@ -3,13 +3,13 @@ class Event {
   String unidadeCurricular;
   String turno;
   String turma;
-  int inscritosNoTurno;
+  String inscritosNoTurno;
   String diaDaSemana;
   String horaInicioAula;
   String horaFimAula;
   String dataAula;
   String salaAtribuidaAAula;
-  int lotacaoSala;
+  String lotacaoSala;
 
   Event(
       this.curso,
@@ -24,7 +24,14 @@ class Event {
       this.salaAtribuidaAAula,
       this.lotacaoSala);
 
+  factory Event.fromCSV(String csv) {
+    var event = csv.split(RegExp(',(?=([^"]*"[^"]*")*[^"]*\$)'));
+
+    return Event(event[0], event[1], event[2], event[3], event[4], event[5],
+        event[6], event[7], event[8], event[9], event[10].trim());
+  }
+
   toJson() {
-    return '{ "Curso": "${curso}", "Unidade Curricular": "${unidadeCurricular}", "Turno": "${turno}", "Turma": "${turma}", "Inscritos no turno": "${inscritosNoTurno}", "Dia da semana": "${diaDaSemana}", "Hora início da aula": "${horaInicioAula}", "Hora fim da aula": "${horaFimAula}", "Data da aula": "${dataAula}", "Sala atribuída à aula": "${salaAtribuidaAAula}", "Lotação da sala": "${lotacaoSala}" }';
+    return '{ "Curso": "${curso.replaceAll('"', "")}", "Unidade Curricular": "${unidadeCurricular.replaceAll('"', "")}", "Turno": "${turno.replaceAll('"', "")}", "Turma": "${turma.replaceAll('"', "")}", "Inscritos no turno": "$inscritosNoTurno", "Dia da semana": "${diaDaSemana.replaceAll('"', "")}", "Hora início da aula": "${horaInicioAula.replaceAll('"', "")}", "Hora fim da aula": "${horaFimAula.replaceAll('"', "")}", "Data da aula": "${dataAula.replaceAll('"', "")}", "Sala atribuída à aula": "${salaAtribuidaAAula.replaceAll('"', "")}", "Lotação da sala": "$lotacaoSala" }';
   }
 }
